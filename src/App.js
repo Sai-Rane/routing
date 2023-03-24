be7b834c1f2c5689aa0018b6860768a1b128fafa
Sai-Rane <sai.rane729@gmail.com>
2023-03-24 18:09:26 +0530

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
 export default App;
+const Home = () => {
+  <>
+    <h1>Home</h1>
+    <h1>Home Page</h1>
+  </>;
+};
+
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <h1>About Page</h1>
+    </>
+  );
+};
+
+const Contact = () => {
+  <>
+    <h1>Contact</h1>
+    <h1>Contact Page</h1>
+  </>;
+};
